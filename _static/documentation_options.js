95300971cc7d5a4c54cbde35c59e178c2864b5c9
vstadnytskyi-FDA <vstadnytskyi-FDA@users.noreply.github.com>
2022-07-30 15:39:26 +0000

    deploy: bc7d2ae08c5aa21d01c23620b6a4fd2fae7db02d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post105+g6e2b40b',
+    VERSION: '0.post116+gbc7d2ae',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
